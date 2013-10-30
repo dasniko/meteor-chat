0900f21e53a1d3baf7b417e812d92d36267c075b
Niko Köbler <niko@n-k.de>
2013-10-30 11:41:21 +0100

    enhanced version to deal with multiple chat-rooms

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 MeteorChat
 ==========
 
-Simple chat application built on the [Meteor](http://www.meteor.com/) JavaScript Platform.
+Simple chat application with different chat-rooms built on the [Meteor](http://www.meteor.com/) JavaScript Platform.
 
 Not much functionality, only to demonstrate how _simple_ it is to set up a reactive app using Meteor.
 
--- meteor-chat.js
+++ meteor-chat.js
@@ -1,9 +1,12 @@
 Messages = new Meteor.Collection("messages");
+Rooms = new Meteor.Collection("rooms");
 
 if (Meteor.isClient) {
     Accounts.ui.config({
         passwordSignupFields: 'USERNAME_ONLY'
     });
+
+    Session.setDefault("roomname", "Meteor");
 
     Template.input.events({
         'click .sendMsg': function(e) {
@@ -18,18 +21,45 @@ if (Meteor.isClient) {
 
     _sendMessage = function() {
         var el = document.getElementById("msg");
-        Messages.insert({user: Meteor.user().username, msg: el.value, ts: new Date()});
+        Messages.insert({user: Meteor.user().username, msg: el.value, ts: new Date(), room: Session.get("roomname")});
         el.value = "";
         el.focus();
     };
 
     Template.messages.messages = function() {
-        return Messages.find({}, {sort: {ts: -1}});
+        return Messages.find({room: Session.get("roomname")}, {sort: {ts: -1}});
     };
+
+    Template.message.timestamp = function() {
+        return this.ts.toLocaleString();
+    }
+
+    Template.messages.roomname = function() {
+        return Session.get("roomname");
+    }
+
+    Template.rooms.events({
+        'click li': function(e) {
+            Session.set("roomname", e.srcElement.innerText);
+        }
+    });
+
+    Template.rooms.rooms = function() {
+        return Rooms.find();
+    }
+
+    Template.room.roomstyle = function() {
+        return Session.equals("roomname", this.roomname) ? "font-weight: bold" : "";
+    }
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-      //Messages.remove({});
-  });
+    Meteor.startup(function () {
+        //Messages.remove({});
+        if (Rooms.find().count() === 0) {
+            ["Meteor", "JavaScript", "Reactive", "MongoDB"].forEach(function(r) {
+                Rooms.insert({roomname: r});
+            });
+        }
+    });
 }
